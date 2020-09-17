b32513a8029707d4a24a5241ab2de2884afdb535
georg3103 <gnmkrtchyan@gmail.com>
2020-09-17 13:47:59 +0300

    Updates

---
+++ precache-manifest.0aaf2cddfc31f13ed0aa0237581090b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e22730bdaa1ce44f124b",
+    "url": "/testDashboard/static/js/0.cc23c633.chunk.js"
+  },
+  {
+    "revision": "c208724ee5af6d3587e2",
+    "url": "/testDashboard/static/css/main.6794920f.chunk.css"
+  },
+  {
+    "revision": "c208724ee5af6d3587e2",
+    "url": "/testDashboard/static/js/main.ee20ffcd.chunk.js"
+  },
+  {
+    "revision": "d97a35edd792b5ef935a",
+    "url": "/testDashboard/static/js/runtime~main.a35e3c00.js"
+  },
+  {
+    "revision": "ceb91702359657a62c23",
+    "url": "/testDashboard/static/js/3.b6e4088c.chunk.js"
+  },
+  {
+    "revision": "a6ccf5eaa893adf7af5d",
+    "url": "/testDashboard/static/js/4.db05694e.chunk.js"
+  },
+  {
+    "revision": "a08610d9871308ffd2dd",
+    "url": "/testDashboard/static/js/5.b1be3de2.chunk.js"
+  },
+  {
+    "revision": "70e465d55e4f2a01ec9b",
+    "url": "/testDashboard/static/js/6.2a70bb53.chunk.js"
+  },
+  {
+    "revision": "38265d302886f10c7516b825920f1b5d",
+    "url": "/testDashboard/index.html"
+  }
+];
